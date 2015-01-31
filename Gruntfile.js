8db5a115ed80f7dc9c7b5802f9b67bea5fe3fd09
Patrick Waivers <pwaivers@gmail.com>
2015-01-31 00:49:27 -0500

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Load all the grunt tasks (use later)
+  require('load-grunt-tasks')(grunt);
+
+  // set variables
+  var allSrc = ['app/**/*', '!app/lib/**/*'];
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: 'app'
+        }
+      }
+    },
+
+    watch: {
+      files: allSrc,
+      tasks: ['jshint'],
+      options: {
+        livereload: true,
+      },
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'app/main.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'app/**/*.js', '!app/lib/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc' // relative to Gruntfile
+      }
+    },
+
+    wiredep: {
+      task: {
+        src: [
+          'app/index.tpl.html',
+        ]
+      }
+    },
+
+    includeSource: {
+      options: {
+        basePath: 'app',
+        baseUrl: ''
+      },
+      myTarget: {
+        files: {
+          'app/index.html': 'app/index.tpl.html'
+        }
+      }
+    }
+  });
+
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['wiredep', 'includeSource', 'connect', 'watch']);
+
+};
---
+++ app/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoAngularSandboxApp
+ * @description
+ * # yoAngularSandboxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoAngularSandboxApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/sandbox', {
+        templateUrl: 'views/sandbox.html',
+        controller: 'SandboxCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
