cd62ec2323c92273e476a2bd83a31de08fd7a5e6
Patrick's Mac Mini <pwaivers@gmail.com>
2015-02-26 20:19:57 -0500

    Minor Changes to the account page.

---
+++ app/account/register/register.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: admin
+ * Date: 2/26/15
+ * Time: 6:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function(){
+'use strict';
+
+angular.module('angularSandboxApp')
+    .controller('registerController', registerController);
+
+function registerController(){
+    var that = this;
+}
+
+}());
